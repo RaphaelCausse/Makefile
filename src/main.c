ae1721347399d6158263d4d31f10e1e94bc3e882
Raphael CAUSSE <raphael.causse2@gmail.com>
2024-02-29 22:58:25 +0100

    update: src template

--- src/main.c
+++ src/main.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
 
-#ifdef _WIN32       // For Windows
-    #include <winsock2.h>
-#endif
-#ifdef __linux__    // For Linux
-    #include <arpa/inet.h>
-#endif
-
-
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Thank you for using my Makefile !\n\n");
-
-#ifdef _WIN32
-    uint32_t word_h = 1;  // Host word, little endian 
-    uint32_t word_n = htonl(word_h);  // Network word, big endian
-    printf("HOST word ->  bytes: %u  -> dec: %10u  -> hex: %08X\n", sizeof(word_h), word_h, word_h);
-    printf("NTWK word ->  bytes: %u  -> dec: %10u  -> hex: %08X\n", sizeof(word_n), word_n, word_n);
-    printf("This is an Endian swap example from Little Endian to Big Endian.\n");
-#endif
-#ifdef __linux__
-    uint32_t word_h = 1;  // Host word, little endian
-    uint32_t word_n = htonl(word_h);  // Network word, big endian
-    printf("HOST word ->  bytes: %lu  -> dec: %10u  -> hex: %08X\n", sizeof(word_h), word_h, word_h);
-    printf("NTWK word ->  bytes: %lu  -> dec: %10u  -> hex: %08X\n", sizeof(word_n), word_n, word_n);
-    printf("This is an Endian swap example from Little Endian to Big Endian.\n");
-#endif
-
+    printf("Hello with my Makefile !\n");
+    for (int i = 0; i < argc; ++i)
+    {
+        printf("argv[%i]: %s\n", i, argv[i]);
+    }
     return 0;
 }
--- src/sources.mk
+++ src/sources.mk
@@ -1,3 +1,4 @@
-# Declare all source files in the SOURCES variable, with "src" as the relative root. 
-# Write source files names on one line, or on multiple lines by adding a backslash at the end and going on a new line. 
-SOURCES := main.c \
+# Declare all source files in the SOURCES variable, with "src" as the relative root.
+# Write source files names on one line, or on multiple lines by adding a backslash at the end and going on a new line (no other characters after the backslash).
+SOURCES := \
+main.c \
